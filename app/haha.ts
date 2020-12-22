fe69601f626df487f91b807a703669f8895dd859
jainshreyp <shrey.jain@snackpass.co>
2020-12-22 10:03:54 -0500

    Changed hahajs to hahats for some ts related funcs

--- app/haha.ts
+++ app/haha.ts
@@ -10,14 +10,24 @@ function multiplyBy2(state = { value: 1}, action) {
     }
 }
 
+function addByValue(state, action) {
+    return(
+        {value: Number(state) + Number(action)}
+    )
+}
+
+const indexReducer = redux.combineReducers({
+    multiplyBy2: multiplyBy2,
+    addByValue: addByValue
+})
 //creates a store (state tree) with a func param, function is the reduction function
-let store = redux.createStore(multiplyBy2)
+let store = redux.createStore(indexReducer)
 
 //subscribes to finding changing in the store, and then performs the arrow function commands
 store.subscribe(() => console.log(store.getState()))
 
 //a bunch of test commands 
-store.dispatch({ type: 'mult' })
+//store.dispatch(addByValue(10,2))
 store.dispatch({ type: 'mult' })
 store.dispatch({ type: 'mult' })
 store.dispatch({ type: 'mult' })
