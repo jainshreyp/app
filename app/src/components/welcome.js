cdae0503f5474a2caa00181522deaf7375e2278c
jainshreyp <shrey.jain@snackpass.co>
2020-12-22 21:14:37 -0500

    Smaller update on the Welcome comp

--- app/src/components/welcome.js
+++ app/src/components/welcome.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Welcome = (props) => {
     return(
-        <div>
-            <p>{props.value[0]}</p>
+        <div className="bg-gray-300 px-auto">
+            <p>{props.value}</p>
             {props.value.forEach(element => {
-            <p>{element}</p>
-        })}
+                <p>{element}</p>
+            })}
         </div>
     )
 }
