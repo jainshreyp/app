b981fbf47d81297ccf0d312e99ae013d22ce0047
jainshreyp <shrey.jain@snackpass.co>
2020-12-24 16:48:42 -0500

    added the checkboxes in md

--- app/src/components/welcome.js
+++ app/src/components/welcome.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Welcome = (props) => {
     return(
         <div className="bg-gray-300 px-auto">
-            <p>{props.value}</p>
-            {props.value.forEach(element => {
-                <p>{element}</p>
-            })}
+            <p>{props.value.map = (elem) => {
+                return(<p className="text-3xl text-blue-500">{elem}</p>)
+            }}</p>
+            <p className="text-3xl text-blue-500">{props.value}</p>
         </div>
     )
 }
