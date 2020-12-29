500e23b462f1e22fc8a2968dcf2b1e3f81bba116
jainshreyp <shrey.jain@snackpass.co>
2020-12-28 19:07:33 -0500

    Save some changes while i delete it

--- source/src/App.js
+++ source/src/App.js
@@ -1,28 +1,30 @@
 import logo from './logo.svg';
-import { useCallback, useState } from 'react'
+import { useState, useEffect, render } from "react"
 import './App.css';
 
 function App() {
-  const [startAction, setStartAction] = useState({
-    payload: true
+  const [speed, setSpeed] = useState(20)
+  useEffect(() => {
+    <p className="text-white">Hello World</p>
   })
-  const [stopAction, setStopAction] = useState({payload:false})
-
+  function buttonClick() {
+    setSpeed(speed-1)
+    render()
+  }
   return (
     <div className="App">
+      <link rel="stylesheet" href="https://unpkg.com/tailwindcss/dist/tailwind.min.css"/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <img src={logo} className="" style={{animation: `App-logo-spin infinite 20s linear`, height: "40vmin"}} alt="logo" />
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <button onClick={buttonClick} className="w-48 h-24 bg-gray-100 text-black rounded-lg">Click me to spin faster</button>
       </header>
     </div>
   );
